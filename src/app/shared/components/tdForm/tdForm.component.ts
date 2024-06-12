78a9d549ca4b8ab0065ca93326e43827e840a0d3
afifakhatib <khatibadiba12@gmail.com>
2024-06-12 12:36:13 +0530

    first commit

---
+++ src/app/shared/components/tdForm/tdForm.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { IcontactMode, IuserInfo } from "../../models/tdForm.interface";
+
+
+
+@Component({
+    selector : 'app-TdForm',
+    templateUrl : './tdForm.component.html',
+    styleUrls : ['./tdForm.component.scss']
+})
+export class TdFormComponent implements OnInit{
+   
+  userInfo : IuserInfo ={
+    userEmail : {
+        username: "iamAkhatib",
+        email: "ak@gmail.com",
+    },
+      contact: 1234567811,
+      secretQ: "Fav Idol",
+      answer: "bts",
+      isSubscribe: false,
+      contactModeArr: "mobile"
+  }
+
+  contactModeArr : Array<IcontactMode> = [
+    {
+        moc : 'mode Email',
+        id : 9597,
+    },
+    {
+        moc : 'mode Mobile',
+        id : 9795
+    }
+  ]
+
+    @ViewChild('signUp') signUp !: NgForm;
+
+    constructor(){}
+
+    ngOnInit(): void {
+    }
+    
+    onSignUp(signUp : NgForm){
+       if(signUp.valid){
+        console.log(signUp);
+        console.log(signUp.value);
+        signUp.reset()
+       }
+    }
+
+    onEditform(){
+        this.signUp.form.patchValue(this.userInfo)
+    }
+}
